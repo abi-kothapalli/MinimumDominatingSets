db2c87ea4c197cd408915d49d94cabfddb0b4b76
Abi Kothapalli <abi.kothapalli@gmail.com>
2022-02-17 00:28:31 -0600

    Redid graph data parsing

--- formatGraph.py
+++ formatGraph.py
@@ -2,7 +2,7 @@ import os
 from os import path
 
 INPUT_PATH = "graphs/matrices"
-OUTPUT_PATH = "graphs/output"
+OUTPUT_PATH = "graphs/graph_data"
 
 def convert_to_edge_list(adj_mat):
   rows = adj_mat.read().split('\n')
@@ -32,19 +32,43 @@ def file_crawler():
 
 def split_graphs(FILE_PATH):
 
-    if not path.isdir(INPUT_PATH):
-        os.mkdir(path.join(os.getcwd(), INPUT_PATH))
-    else:
-        return
+    if not path.isdir(path.join(os.getcwd(), OUTPUT_PATH)):
+        os.mkdir(path.join(os.getcwd(), OUTPUT_PATH))
 
     with open(FILE_PATH) as file:
-        graphs = file.read().split("\n\n")
+        graphs = file.read().split("\n\n\n\n")
         for i, graph in enumerate(graphs):
             if graph:
-                with open(path.join(os.getcwd(), INPUT_PATH, f"graph{i + 1}.txt"), "w") as output:
-                    output.write(graph)
+
+                adj_list, graph_data = graph.split("\n\n")
+                mds = graph_data[graph_data.find("Minimum Dominating Set: ") + len("Minimum Dominating Set: ")]
+
+                lists = adj_list.split("\n")
+
+                edge_list = []
+
+                for node in lists:
+                    node_split = node.split(": ")
+
+                    if len(node_split) > 1:
+                        node, neighbors = node_split
+                    else:
+                        edge_list.append(f"{node} {node}")
+                        continue
+
+                    neighbors = neighbors.split(" ")
+
+                    node = int(node)
+                    for neighbor in neighbors:
+                        neighbor = int(neighbor)
+
+                        if neighbor > node:
+                            edge_list.append(f"{node} {neighbor}")
+
+                with open(path.join(os.getcwd(), OUTPUT_PATH, f"graph{(i + 1):04}.txt"), "w") as output:
+                    output.write(f"{mds}\n")
+                    output.write("\n".join(edge_list))
 
 
 if __name__ == "__main__":
-    split_graphs(path.join(os.getcwd(), "graphs/list_182_graphs.mat"))
-    file_crawler()
+    split_graphs(path.join(os.getcwd(), "graphs/list_182_graphs.txt"))
